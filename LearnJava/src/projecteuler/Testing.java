4f296267824e39ca204f85fccedeb867095f6345
SP <SP@192.168.2.3>
2017-03-27 23:18:38 +0530

    updated

--- LearnJava/src/projecteuler/Testing.java
+++ LearnJava/src/projecteuler/Testing.java
@@ -5,7 +5,7 @@ public class Testing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		for (long i = 11l; i < 4000000l; i++) {
+		for (int i = 11; i < 2147483647; i++) {
 			if (i % 1 == 0 && i % 2 == 0 && i % 3 == 0 && i % 4 == 0
 					&& i % 5 == 0 && i % 6 == 0 && i % 7 == 0 && i % 8 == 0
 					&& i % 9 == 0 && i % 10 == 0 && i % 11 == 0 && i % 12 == 0
